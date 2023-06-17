613258c8bf22e5624ba231c535d8b98c368833ca
nDm-786 <ndmbhattiofficial@gmail.com>
2023-06-17 21:35:20 +0500

    ...

---
+++ School Management System/School Management System/AdminDashboardTab.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace School_Management_System
+{
+    partial class AdminDashboardTab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboardTab));
+            this.ExamsTab = new System.Windows.Forms.Label();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.EmployeesTab = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.guna2PictureBox5 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EmployeeTab = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.StudentsTab = new System.Windows.Forms.Panel();
+            this.StudentTab = new System.Windows.Forms.Label();
+            this.ExamTab = new System.Windows.Forms.Panel();
+            this.guna2PictureBox6 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.DashboardTab = new System.Windows.Forms.Panel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.guna2PictureBox4 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LogoutTab = new System.Windows.Forms.Panel();
+            this.LogoutBtn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox5)).BeginInit();
+            this.EmployeeTab.SuspendLayout();
+            this.StudentsTab.SuspendLayout();
+            this.ExamTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox6)).BeginInit();
+            this.DashboardTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox4)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.LogoutTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ExamsTab
+            // 
+            this.ExamsTab.AutoSize = true;
+            this.ExamsTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExamsTab.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ExamsTab.Location = new System.Drawing.Point(65, 13);
+            this.ExamsTab.Name = "ExamsTab";
+            this.ExamsTab.Size = new System.Drawing.Size(73, 24);
+            this.ExamsTab.TabIndex = 0;
+            this.ExamsTab.Text = "Exams";
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox2.Image")));
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(12, 2);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.Size = new System.Drawing.Size(52, 47);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.guna2PictureBox2.TabIndex = 1;
+            this.guna2PictureBox2.TabStop = false;
+            // 
+            // guna2PictureBox3
+            // 
+            this.guna2PictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox3.Image")));
+            this.guna2PictureBox3.ImageRotate = 0F;
+            this.guna2PictureBox3.Location = new System.Drawing.Point(12, 2);
+            this.guna2PictureBox3.Name = "guna2PictureBox3";
+            this.guna2PictureBox3.Size = new System.Drawing.Size(52, 47);
+            this.guna2PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.guna2PictureBox3.TabIndex = 1;
+            this.guna2PictureBox3.TabStop = false;
+            // 
+            // EmployeesTab
+            // 
+            this.EmployeesTab.AutoSize = true;
+            this.EmployeesTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeesTab.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.EmployeesTab.Location = new System.Drawing.Point(65, 13);
+            this.EmployeesTab.Name = "EmployeesTab";
+            this.EmployeesTab.Size = new System.Drawing.Size(114, 24);
+            this.EmployeesTab.TabIndex = 0;
+            this.EmployeesTab.Text = "Employees";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.Controls.Add(this.guna2PictureBox5);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(1, 304);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(212, 52);
+            this.panel4.TabIndex = 5;
+            // 
+            // guna2PictureBox5
+            // 
+            this.guna2PictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox5.Image")));
+            this.guna2PictureBox5.ImageRotate = 0F;
+            this.guna2PictureBox5.Location = new System.Drawing.Point(12, 2);
+            this.guna2PictureBox5.Name = "guna2PictureBox5";
+            this.guna2PictureBox5.Size = new System.Drawing.Size(52, 47);
+            this.guna2PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.guna2PictureBox5.TabIndex = 1;
+            this.guna2PictureBox5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(65, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Time Table";
+            // 
+            // EmployeeTab
+            // 
+            this.EmployeeTab.Controls.Add(this.guna2PictureBox3);
+            this.EmployeeTab.Controls.Add(this.EmployeesTab);
+            this.EmployeeTab.Location = new System.Drawing.Point(1, 251);
+            this.EmployeeTab.Name = "EmployeeTab";
+            this.EmployeeTab.Size = new System.Drawing.Size(212, 52);
+            this.EmployeeTab.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Montserrat", 20F, System.Drawing.FontStyle.Bold);
+            this.label9.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label9.Location = new System.Drawing.Point(325, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(174, 37);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Dashboard";
+            // 
+            // StudentsTab
+            // 
+            this.StudentsTab.BackColor = System.Drawing.Color.Transparent;
+            this.StudentsTab.Controls.Add(this.guna2PictureBox2);
+            this.StudentsTab.Controls.Add(this.StudentTab);
+            this.StudentsTab.Location = new System.Drawing.Point(0, 199);
+            this.StudentsTab.Name = "StudentsTab";
+            this.StudentsTab.Size = new System.Drawing.Size(212, 52);
+            this.StudentsTab.TabIndex = 3;
+            // 
+            // StudentTab
+            // 
+            this.StudentTab.AutoSize = true;
+            this.StudentTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentTab.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.StudentTab.Location = new System.Drawing.Point(65, 13);
+            this.StudentTab.Name = "StudentTab";
+            this.StudentTab.Size = new System.Drawing.Size(91, 24);
+            this.StudentTab.TabIndex = 0;
+            this.StudentTab.Text = "Students";
+            // 
+            // ExamTab
+            // 
+            this.ExamTab.BackColor = System.Drawing.Color.Transparent;
+            this.ExamTab.Controls.Add(this.guna2PictureBox6);
+            this.ExamTab.Controls.Add(this.ExamsTab);
+            this.ExamTab.Location = new System.Drawing.Point(2, 356);
+            this.ExamTab.Name = "ExamTab";
+            this.ExamTab.Size = new System.Drawing.Size(212, 52);
+            this.ExamTab.TabIndex = 6;
+            // 
+            // guna2PictureBox6
+            // 
+            this.guna2PictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox6.Image")));
+            this.guna2PictureBox6.ImageRotate = 0F;
+            this.guna2PictureBox6.Location = new System.Drawing.Point(12, 2);
+            this.guna2PictureBox6.Name = "guna2PictureBox6";
+            this.guna2PictureBox6.Size = new System.Drawing.Size(52, 47);
+            this.guna2PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.guna2PictureBox6.TabIndex = 1;
+            this.guna2PictureBox6.TabStop = false;
+            // 
+            // DashboardTab
+            // 
+            this.DashboardTab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(12)))), ((int)(((byte)(63)))));
+            this.DashboardTab.Controls.Add(this.guna2PictureBox1);
+            this.DashboardTab.Controls.Add(this.label1);
+            this.DashboardTab.Location = new System.Drawing.Point(0, 147);
+            this.DashboardTab.Name = "DashboardTab";
+            this.DashboardTab.Size = new System.Drawing.Size(213, 52);
+            this.DashboardTab.TabIndex = 2;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(52, 46);
+            this.guna2PictureBox1.TabIndex = 1;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(65, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dashboard";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(12)))), ((int)(((byte)(63)))));
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Location = new System.Drawing.Point(213, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(814, 41);
+            this.panel2.TabIndex = 57;
+            // 
+            // guna2PictureBox4
+            // 
+            this.guna2PictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox4.FillColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox4.Image")));
+            this.guna2PictureBox4.ImageRotate = 0F;
+            this.guna2PictureBox4.Location = new System.Drawing.Point(0, 1);
+            this.guna2PictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.guna2PictureBox4.Name = "guna2PictureBox4";
+            this.guna2PictureBox4.Size = new System.Drawing.Size(213, 101);
+            this.guna2PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox4.TabIndex = 1;
+            this.guna2PictureBox4.TabStop = false;
+            this.guna2PictureBox4.UseTransparentBackground = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.guna2PictureBox4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(213, 103);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(8)))), ((int)(((byte)(43)))));
+            this.panel1.Controls.Add(this.ExamTab);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.EmployeeTab);
+            this.panel1.Controls.Add(this.StudentsTab);
+            this.panel1.Controls.Add(this.DashboardTab);
+            this.panel1.Controls.Add(this.LogoutTab);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(213, 555);
+            this.panel1.TabIndex = 56;
+            // 
+            // LogoutTab
+            // 
+            this.LogoutTab.Controls.Add(this.LogoutBtn);
+            this.LogoutTab.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LogoutTab.Location = new System.Drawing.Point(0, 505);
+            this.LogoutTab.Name = "LogoutTab";
+            this.LogoutTab.Size = new System.Drawing.Size(213, 50);
+            this.LogoutTab.TabIndex = 1;
+            // 
+            // LogoutBtn
+            // 
+            this.LogoutBtn.AutoSize = true;
+            this.LogoutBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoutBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.LogoutBtn.Location = new System.Drawing.Point(65, 11);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(74, 24);
+            this.LogoutBtn.TabIndex = 2;
+            this.LogoutBtn.Text = "Logout";
+            // 
+            // AdminDashboardTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 555);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "AdminDashboardTab";
+            this.Text = "AdminDashboardTab";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox5)).EndInit();
+            this.EmployeeTab.ResumeLayout(false);
+            this.EmployeeTab.PerformLayout();
+            this.StudentsTab.ResumeLayout(false);
+            this.StudentsTab.PerformLayout();
+            this.ExamTab.ResumeLayout(false);
+            this.ExamTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox6)).EndInit();
+            this.DashboardTab.ResumeLayout(false);
+            this.DashboardTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox4)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.LogoutTab.ResumeLayout(false);
+            this.LogoutTab.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ExamsTab;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
+        private System.Windows.Forms.Label EmployeesTab;
+        private System.Windows.Forms.Panel panel4;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel EmployeeTab;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel StudentsTab;
+        private System.Windows.Forms.Label StudentTab;
+        private System.Windows.Forms.Panel ExamTab;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox6;
+        private System.Windows.Forms.Panel DashboardTab;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel LogoutTab;
+        private System.Windows.Forms.Label LogoutBtn;
+    }
+}
